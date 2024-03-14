81e8378bf81ba4c279a4cba584eb6f074de2a31c
andicretu <149256225+andicretu@users.noreply.github.com>
2024-03-14 10:09:22 +0100

    Update script.js
    Single check functions for both players.

--- script.js
+++ script.js
@@ -18,7 +18,6 @@ let player2Positions = [];
 let plmessage;
 let message;
 let min = [];
-
 function markCell(index) {
     let column = index;
     let row = min[index];
@@ -34,21 +33,27 @@ function markCell(index) {
         player2Positions.push([column, row]);
     }
     ++min[index];
-    checkVerticalsPlayer1();
-    checkVerticalsPlayer2();
-    checkHorizontalsPlayer1();
-    checkHorizontalsPlayer2();
-    checkDiagonalsPlayer1();
-    checkDiagonalsPlayer2();
+    checkVerticals();
+    checkHorizontals();
+    checkDiagonals()
 }
 
 let player1 = "🔴"
 let player2 = "🟡";
 let player = player1;
-
 function togglePlayers() {
     player = player === player1 ? player2 : player1;
+    togglePositions();
     return player;
+}
+
+let currentPlayerPositions = [];
+function togglePositions() {
+    if (player === player1) {
+        currentPlayerPositions = player1Positions;
+    } else {
+        currentPlayerPositions = player2Positions;
+    }
 }
 
 function comparePairsVertical(a, b) {
@@ -65,132 +70,67 @@ function comparePairsHorizontal(a, b) {
     return a[0] - b[0];
 }
 
-function checkVerticalsPlayer1() {
-    let length = player1Positions.length;
-    player1Positions.sort(comparePairsVertical);
-    let consecutiveV = 0;  
-    for (let i = 0; i < length - 1; ++i) {
-        if (player1Positions[i][0] === player1Positions[i + 1][0] && player1Positions[i][1] - player1Positions[i + 1][1] === -1) {
-            ++consecutiveV;
-        } else {
-            consecutiveV = 0;
-        }
-        if (consecutiveV === win) {
-            winMessage(player1);
-            break; 
-        }
-    }
-}
-
-function checkVerticalsPlayer2() {
-    let length = player2Positions.length;
-    player2Positions.sort(comparePairsVertical);
+function checkVerticals() {
+    let length = currentPlayerPositions.length;
+    currentPlayerPositions.sort(comparePairsVertical);
     let consecutiveV = 0;
     for (let i = 0; i < length - 1; ++i) {
-        if (player2Positions[i][0] === player2Positions[i + 1][0] && player2Positions[i][1] - player2Positions[i + 1][1] === -1) {
+        if (currentPlayerPositions[i][0] === currentPlayerPositions[i + 1][0] && currentPlayerPositions[i][1] - currentPlayerPositions[i + 1][1] === -1) {
             ++consecutiveV;
         } else {
             consecutiveV = 0;
         }
         if (consecutiveV === win) {
-            winMessage(player2);
+            winMessage(player);
             break;
         }
     }
 }
 
-function checkHorizontalsPlayer1() {
-    let length = player1Positions.length;
-    player1Positions.sort(comparePairsHorizontal);
+function checkHorizontals() {
+    let length = currentPlayerPositions.length;
+    currentPlayerPositions.sort(comparePairsHorizontal);
     let consecutiveH = 0;
     for (let i = 0; i < length - 1; ++i) {
-        if (player1Positions[i][1] === player1Positions[i + 1][1] && player1Positions[i][0] - player1Positions[i + 1][0] == -1) {
+        if (currentPlayerPositions[i][1] === currentPlayerPositions[i + 1][1] && currentPlayerPositions[i][0] - currentPlayerPositions[i + 1][0] == -1) {
             ++consecutiveH;
         } else {
             consecutiveH = 0;
         }
         if (consecutiveH === win) {
-            winMessage(player1);
+            winMessage(player);
             break; 
         }
-    }
+    }  
 }
 
-function checkHorizontalsPlayer2() {
-    let length = player2Positions.length;
-    player2Positions.sort(comparePairsHorizontal);
-    let consecutiveH = 0;  
-    for (let i = 0; i < length - 1; ++i) {
-        if (player2Positions[i][1] === player2Positions[i + 1][1] && player2Positions[i][0] - player2Positions[i + 1][0] === -1) {
-            ++consecutiveH;
-        } else {
-            consecutiveH = 0;
-        }
-        if (consecutiveH === win) {
-            winMessage(player2);
-            break;
-        }
-    }
-}
-
-function checkDiagonalsPlayer1() {
-    let length = player1Positions.length;
-    player1Positions.sort(comparePairsVertical);
+function checkDiagonals() {
+    let length = currentPlayerPositions.length;
+    currentPlayerPositions.sort(comparePairsVertical);
     for (let i = 0; i < length; ++i) {
         let consecutiveD = 0;
         for (let j = i + 1; j < length; ++j) {
-            let d1 = player1Positions[j][1] - player1Positions[i][1];
-            let d0 = player1Positions[j][0] - player1Positions[i][0];
+            let d1 = currentPlayerPositions[j][1] - currentPlayerPositions[i][1];
+            let d0 = currentPlayerPositions[j][0] - currentPlayerPositions[i][0];
             if (d1 === d0 && d1 === consecutiveD + 1) {
                 ++consecutiveD;
             }
         }
         if (consecutiveD === win) {
-            winMessage(player1);
+            winMessage(player);
             break; 
         }
         consecutiveD = 0;
         for (let j = i + 1; j < length; ++j) {
-            let d1 = player1Positions[j][1] - player1Positions[i][1];
-            let d0 = player1Positions[j][0] - player1Positions[i][0];
+            let d1 = currentPlayerPositions[j][1] - currentPlayerPositions[i][1];
+            let d0 = currentPlayerPositions[j][0] - currentPlayerPositions[i][0];
             if (d1 === -d0 && d0 === consecutiveD + 1) {
                 ++consecutiveD;
             }
         }
         if (consecutiveD === win) {
-            winMessage(player1);
+            winMessage(player);
             break; 
-        }
-    }
-}
-
-function checkDiagonalsPlayer2() {
-    let length = player2Positions.length;
-    player1Positions.sort(comparePairsVertical);
-    for (let i = 0; i < length; ++i) {
-        let consecutiveD = 0;
-        for (let j = i + 1; j < length; ++j) {
-            let d1 = player2Positions[j][1] - player2Positions[i][1];
-            let d0 = player2Positions[j][0] - player2Positions[i][0]
-            if (d1 === d0 && d1 === consecutiveD + 1) {
-                ++consecutiveD;
-            }
-        }
-        if (consecutiveD === win) {
-            winMessage(player2);
-            break;
-        }
-        consecutiveD = 0;
-        for (let j = i + 1; j < length; ++j) {
-            d1 = player2Positions[j][1] - player2Positions[i][1];
-            d0 = player2Positions[j][0] - player2Positions[i][0];
-            if (d1 === -d0 && d1 === consecutiveD + 1) { 
-                ++consecutiveD;
-            }
-        }
-        if (consecutiveD === win) {
-            winMessage(player2);
-            break;
         }
     }
 }
@@ -198,6 +138,7 @@ function checkDiagonalsPlayer2() {
 function winMessage(player) {
     plmessage = document.getElementById("winMessage");
     plmessage.textContent = player + " wins!!";
+    initiateRowAndPlayers();
 }
 
 let columns = [];
